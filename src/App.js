94bae35282c64509a43062e5982eb4a0c9b3c289
Giancucinotta <giancucinotta@gmail.com>
2021-07-15 21:33:26 -0300

    Landing background + CSS feed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
+import MyComponent from './components/Vanta';
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    if(!localStorage.getItem('username')) return <MyComponent />
+
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="aa5a6a06-fb21-4251-a348-8f4fe461449a"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps}/>}
+        />
+    );
 }
 
 export default App;
---
+++ src/components/Vanta.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from 'react'
+import BIRDS from 'vanta/dist/vanta.ripple.min'
+import LoginForm from './LoginForm'
+// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
+
+const MyComponent = (props) => {
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const myRef = useRef(null)
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(BIRDS({
+        el: myRef.current
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+  return <div ref={myRef}>
+    <LoginForm/>
+  </div>
+}
+
+export default MyComponent;
