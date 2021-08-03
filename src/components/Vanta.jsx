a027a315e27cdaaa2cf30148ca7600a7b0ee2ab3
Giancucinotta <giancucinotta@gmail.com>
2021-08-03 14:15:50 -0300

    -

--- src/components/Vanta.jsx
+++ src/components/Vanta.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import BIRDS from 'vanta/dist/vanta.ripple.min'
 import LoginForm from './LoginForm'
-// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
 
 const MyComponent = (props) => {
   const [vantaEffect, setVantaEffect] = useState(0)
